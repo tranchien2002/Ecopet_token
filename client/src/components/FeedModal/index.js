f1d339f4aded3718ce2f6a1d1efa70a5ef216fee
tran.minh.chien <SUN-ASTERISK\tran.minh.chien@b121716-pc.sun-asterisk.com>
2019-10-02 13:26:06 +0700

    noted

--- client/src/components/FeedModal/index.js
+++ client/src/components/FeedModal/index.js
@@ -15,13 +15,15 @@ import {
 } from 'reactstrap';
 import Token from 'constants/Token.js';
 import './index.css';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
 class FeedPetModal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isDropdownOpen: false,
       currentToken: '',
-      name: 'ETH',
+      nameToken: 'ETH',
       tokenAddress: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
       value: 1,
       tokenValue: 0,
@@ -35,7 +37,7 @@ class FeedPetModal extends React.Component {
     this.setState({
       value: props.value,
       currentToken: 'https://files.kyber.network/DesignAssets/tokens/eth.svg',
-      name: 'ETH'
+      nameToken: 'ETH'
     });
   }
   handelDropdownToggle = () => {
@@ -43,10 +45,10 @@ class FeedPetModal extends React.Component {
       isDropdownOpen: !this.state.isDropdownOpen
     });
   };
-  changeToken = async (src, name, address) => {
+  changeToken = async (src, nameToken, address) => {
     this.setState({
       currentToken: src,
-      name: name,
+      nameToken: nameToken,
       tokenAddress: address
     });
     await this.getRate();
@@ -57,10 +59,10 @@ class FeedPetModal extends React.Component {
   getRate = async () => {
     let ratesRequest = await fetch('https://api.kyber.network/change24h');
     let rateList = await ratesRequest.json();
-    let rate = rateList['ETH_' + this.state.name].rate_eth_now;
-    let rateUSD = rateList['ETH_' + this.state.name].rate_usd_now;
-    let changeETH = rateList['ETH_' + this.state.name].change_eth_24h;
-    let changeUSD = rateList['ETH_' + this.state.name].change_usd_24h;
+    let rate = rateList['ETH_' + this.state.nameToken].rate_eth_now;
+    let rateUSD = rateList['ETH_' + this.state.nameToken].rate_usd_now;
+    let changeETH = rateList['ETH_' + this.state.nameToken].change_eth_24h;
+    let changeUSD = rateList['ETH_' + this.state.nameToken].change_usd_24h;
     this.setState({
       rate: rate,
       changeETH: changeETH,
@@ -68,12 +70,22 @@ class FeedPetModal extends React.Component {
       rateUSD: rateUSD
     });
   };
-  handleChange = (event) => {
+  handleChange = async (event) => {
     this.getRate();
     this.setState({
       tokenValue: event.target.value,
       value: event.target.value * this.state.rate
     });
+    let ratesRequest = await fetch(
+      'https://ropsten-api.kyber.network/buy_rate?id=' +
+        this.state.tokenAddress +
+        '&qty=' +
+        event.target.value * this.state.rate
+    );
+    // Parsing the output
+    let rates = await ratesRequest.json();
+    // Getting the source quantity
+    console.log(rates);
   };
   handleChangeETH = (event) => {
     this.getRate();
@@ -82,8 +94,89 @@ class FeedPetModal extends React.Component {
       tokenValue: event.target.value / this.state.rate
     });
   };
-  handleSwapTokenClick = (value) => () => {
-    //TODO: swap token
+  handleSwapTokenClick = (value) => async () => {
+    const ETH_TOKEN_ADDRESS = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
+    // How many KNC you want to buy
+    const QTY = 1000;
+    // Gas price of the transaction
+    const GAS_PRICE = 'medium';
+    // Your Ethereum wallet addr
+    const USER_ACCOUNT = this.props.tomo.account;
+
+    let tokenInfoRequest = await fetch('https://ropsten-api.kyber.network/currencies');
+
+    let tokens = await tokenInfoRequest.json();
+    // Checking to see if KNC is supported
+    let supported = tokens.data.some((token) => {
+      return this.state.nameToken === token.symbol;
+    });
+    // If not supported, return.
+    if (!supported) {
+      console.log('Token is not supported');
+      return;
+    }
+
+    let enabledStatusesRequest = await fetch(
+      'https://ropsten-api.kyber.network/users/' + USER_ACCOUNT + '/currencies'
+    );
+    // Parsing the output
+    let enabledStatuses = await enabledStatusesRequest.json();
+    // Checking to see if DAI is enabled
+    let enabled = enabledStatuses.data.some((token) => {
+      if (token.id == this.state.nameToken.toLowerCase()) {
+        return token.enabled;
+      }
+    });
+
+    if (!enabled) {
+      // Querying the API /users/<user_address>/currencies/<currency_id>/enable_data?gas_price=<gas_price> endpoint
+      let enableTokenDetailsRequest = await fetch(
+        'https://ropsten-api.kyber.network/users/' +
+          USER_ACCOUNT +
+          '/currencies/' +
+          this.state.nameToken +
+          '/enable_data?gas_price=' +
+          GAS_PRICE
+      );
+
+      // Parsing the output
+      let enableTokenDetails = await enableTokenDetailsRequest.json();
+      // Extract the raw transaction details
+      let rawTx = enableTokenDetails.data;
+      // Broadcasting the transaction
+      await this.props.tomo.web3.eth.sendTransaction(rawTx).catch((error) => console.log(error));
+    }
+
+    let ratesRequest = await fetch(
+      'https://ropsten-api.kyber.network/buy_rate?id=' + this.state.tokenAddress + '&qty=' + QTY
+    );
+    // Parsing the output
+    let rates = await ratesRequest.json();
+    // Getting the source quantity
+    console.log(rates);
+    let srcQty = rates.data[0].src_qty;
+    console.log(srcQty);
+
+    let tradeDetailsRequest = await fetch(
+      'https://ropsten-api.kyber.network/trade_data?user_address=' +
+        USER_ACCOUNT +
+        '&src_id=' +
+        ETH_TOKEN_ADDRESS +
+        '&dst_id=' +
+        this.state.tokenAddress +
+        '&src_qty=' +
+        srcQty / 0.97 +
+        '&min_dst_qty=' +
+        QTY +
+        '&gas_price=' +
+        GAS_PRICE +
+        '&destAddress=0xBF1CF3939a12239829754Cf208B649b45FD1BE9e'
+    );
+    let tradeDetails = await tradeDetailsRequest.json();
+    // Extract the raw transaction details
+    let rawTx = tradeDetails.data[0];
+    await this.props.tomo.web3.eth.sendTransaction(rawTx);
+    console.log(rawTx);
     this.props.toggle();
   };
   render() {
@@ -110,7 +203,7 @@ class FeedPetModal extends React.Component {
                       <Col>
                         <img src={this.state.currentToken} width='28' alt='Token' />
                       </Col>
-                      <Col>{this.state.name}</Col>
+                      <Col>{this.state.nameToken}</Col>
                     </Row>
                   </DropdownToggle>
                   <DropdownMenu>
@@ -163,7 +256,7 @@ class FeedPetModal extends React.Component {
               <Col></Col>
               <Col>
                 <div className='rate'>
-                  1 {this.state.name} = {Math.round(this.state.rate * 1000) / 1000} ETH ={' '}
+                  1 {this.state.nameToken} = {Math.round(this.state.rate * 1000) / 1000} ETH ={' '}
                   {Math.round(this.state.rateUSD * 1000) / 1000} USD
                 </div>
               </Col>
@@ -200,4 +293,10 @@ class FeedPetModal extends React.Component {
     );
   }
 }
-export default FeedPetModal;
+const mapStateToProps = (state) => {
+  return {
+    tomo: state.tomo
+  };
+};
+
+export default compose(connect(mapStateToProps))(FeedPetModal);
--- client/src/constants/Token.js
+++ client/src/constants/Token.js
@@ -2,27 +2,17 @@ const Token = [
   {
     src: 'https://files.kyber.network/DesignAssets/tokens/knc.svg',
     name: 'KNC',
-    address: '0xdd974D5C2e2928deA5F71b9825b8b646686BD200'
+    address: '0x4E470dc7321E84CA96FcAEDD0C8aBCebbAEB68C6'
   },
   {
     src: 'https://files.kyber.network/DesignAssets/tokens/dai.svg',
     name: 'DAI',
-    address: '0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359'
+    address: '0xaD6D458402F60fD3Bd25163575031ACDce07538D'
   },
   {
     src: 'https://files.kyber.network/DesignAssets/tokens/link.svg',
     name: 'LINK',
-    address: '0x514910771AF9Ca656af840dff83E8264EcF986CA'
-  },
-  {
-    src: 'https://files.kyber.network/DesignAssets/tokens/mln.svg',
-    name: 'MLN',
-    address: '0xec67005c4E498Ec7f55E092bd1d35cbC47C91892'
-  },
-  {
-    src: 'https://files.kyber.network/DesignAssets/tokens/wbtc.svg',
-    name: 'WBTC',
-    address: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'
+    address: '0xb4f7332ed719Eb4839f091EDDB2A3bA309739521'
   }
 ];
 export default Token;
